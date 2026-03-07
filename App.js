a24cf3d26361f83f8a84a2df05c02d4587c5def2
Santiago Aristizabal <aristizabalsantiago482@gmail.com>
2026-03-07 06:38:55 -0500

    feat: add navigation and firebase integration
    - Updated package.json to include react-navigation and firebase dependencies.
    - Created firebase.js for Firebase initialization and authentication.
    - Added Home screen with navigation to Details screen.
    - Created Details screen for displaying details.

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+import { Button, Text, TextInput, View } from "react-native";
+import { auth } from "./firebase";
 
 export default function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignUp = () => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.info("Usuario registrado:", userCredential.user);
+      })
+      .catch((error) => console.log("Error:", error.message));
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ padding: 20 }}>
+      <Text>Email:</Text>
+      <TextInput
+        onChangeText={setEmail}
+        style={{ borderWidth: 1, marginBottom: 10 }}
+      />
+      <Text>Contraseña:</Text>
+      <TextInput
+        secureTextEntry
+        onChangeText={setPassword}
+        style={{
+          borderWidth: 1,
+          marginBottom: 10,
+        }}
+      />
+      <Button title="Registrarse" onPress={handleSignUp} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
