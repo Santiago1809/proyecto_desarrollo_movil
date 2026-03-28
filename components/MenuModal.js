6d85382253096dd68956593b74467c45a26d8fa8
Santiago Aristizabal <aristizabalsantiago482@gmail.com>
2026-03-28 17:25:24 -0500

    Hacer overlay del MenuModal transparente

--- components/MenuModal.js
+++ components/MenuModal.js
@@ -119,7 +119,7 @@ const styles = StyleSheet.create({
   },
   overlay: {
     ...StyleSheet.absoluteFillObject,
-    backgroundColor: "rgba(0,0,0,0.8)",
+    backgroundColor: "transparent",
   },
   drawerContainer: {
     position: "absolute",
